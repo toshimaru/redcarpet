50153a60a785fc7c7e27921d9df30a62e4f239a5
toshimaru <me@toshimaru.net>
2017-03-09 11:41:10 +0900

    Use size instead

--- ext/redcarpet/html.c
+++ ext/redcarpet/html.c
@@ -309,7 +309,6 @@ rndr_header_anchor(struct buf *out, const struct buf *anchor)
 		out->size--;
 
 	// if anchor found empty, use djb2 hash for it
-	if (!inserted && anchor->size) {
 	        unsigned long hash = 5381;
 		for (i = 0; i < size; ++i) {
 			hash = ((hash << 5) + hash) + a[i]; /* h * 33 + c */
